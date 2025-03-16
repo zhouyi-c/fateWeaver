e422040ac7e16ed0ae6f9000ee7cc97b486a76da
zhouyi-c <zhouchi.stu@gmail.com>
2025-03-16 21:45:56 +0800

    first index successful

--- config/base.py
+++ config/base.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-
+    "fate",
 ]
 
 MIDDLEWARE = [
--- fate/views.py
+++ fate/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = "fate/index.html"  # 指定模板路径
+    
+    # 如需传递数据到模板
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tech_stack'] = [
+            {'name': 'Django', 'icon': 'fa-server'},
+            {'name': 'Vue', 'icon': 'fa-vuejs'},
+            {'name': 'MySQL', 'icon': 'fa-database'}
+        ]
+        return context
