86a2cd097539362c3b2a76d1ca167f06417f2464
zhouyi-c <zhouchi.stu@gmail.com>
2025-03-17 09:21:32 +0800

    first from github poll project for my indexpage

--- config/base.py
+++ config/base.py
@@ -127,6 +127,10 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "fate" / "static",
+]  # 静态文件目录
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- fate/views.py
+++ fate/views.py
@@ -3,12 +3,11 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "fate/index.html"  # 指定模板路径
     
-    # 如需传递数据到模板
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tech_stack'] = [
-            {'name': 'Django', 'icon': 'fa-server'},
-            {'name': 'Vue', 'icon': 'fa-vuejs'},
-            {'name': 'MySQL', 'icon': 'fa-database'}
+            {'name': 'Django', 'icon': 'https://img.icons8.com/color/48/000000/django.png', 'description': '后端开发框架'},
+            {'name': 'Vue.js', 'icon': 'https://img.icons8.com/color/48/000000/vuejs.png', 'description': '前端框架'},
+            {'name': 'MySQL', 'icon': 'https://img.icons8.com/color/48/000000/mysql.png', 'description': '数据库系统'}
         ]
         return context
